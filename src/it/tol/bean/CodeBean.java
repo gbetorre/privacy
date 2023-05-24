131f69282fe0c0cc96fd4bd1149dc4228a685bed
gbetorre <gianroberto.torre@gmail.com>
2023-05-24 12:08:31 +0200

    Aggiornata licenza; annotation

--- src/it/tol/bean/CodeBean.java
+++ src/it/tol/bean/CodeBean.java
@@ -12,7 +12,7 @@
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
+ *   the Free Software Foundation; either version 3 of the License, or
  *   (at your option) any later version.
  *
  *   This program is distributed in the hope that it will be useful,
@@ -139,7 +139,6 @@ public class CodeBean implements Serializable {
      * @param o oggetto CodeBean i cui valori devono essere copiati
      * @throws AttributoNonValorizzatoException se l'identificativo dell'oggetto passato come argomento non e' valorizzato!
      */
-    @SuppressWarnings("unused")
     public CodeBean(CodeBean o) 
              throws AttributoNonValorizzatoException {
         // Ammette la mancata valorizzazione di quasi tutti i valori, ma non transige sull'id!
